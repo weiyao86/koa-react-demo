ffdaf2a453b9c3912d6305a0219a54d9ea4fdcd9
weiyao86 <yaowei@goodsogood.com>
2021-04-01 14:20:52 +0800

    add custom plugin

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackBar = require('webpackbar');
+const chalk = require('chalk'); /* console 颜色 */
+const slog = require('single-line-log'); /* 单行打印 console */
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
@@ -39,8 +41,40 @@ function copyFolder(from, to) {
 //自定义插件
 const pluginName = 'ConsoleLogOnBuildWebpackPlugin';
 class ConsoleLogOnBuildWebpackPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  beginCompile() {
+    const lineSlog = slog.stdout;
+    let text = '开始编译：';
+    /* 记录开始时间 */
+    this.starTime = new Date().getTime();
+    this.timer = setInterval(() => {
+      text += '█';
+      lineSlog(chalk.green(text));
+    }, 50);
+  }
+
   //实现apply
   apply(compiler) {
+    /**
+     * Monitor file change 记录当前改动文件
+     */
+    compiler.hooks.watchRun.tap(pluginName, (watching) => {
+      const changeFiles = watching.watchFileSystem.watcher.mtimes;
+      for (let file in changeFiles) {
+        console.log(chalk.green('当前改动文件：' + file));
+      }
+    });
+
+    /**
+     *  before a new compilation is created. 开始 compilation 编译 。
+     */
+    compiler.hooks.compile.tap(pluginName, () => {
+      this.beginCompile();
+    });
+
     compiler.hooks.run.tap(pluginName, (compilation) => {
       console.log('webpack 构建过程开始！');
     });
@@ -55,6 +89,11 @@ class ConsoleLogOnBuildWebpackPlugin {
     //在 webpack 选项中的
     compiler.hooks.done.tap(pluginName, (compilation) => {
       console.log('webpack done ============================>');
+      this.timer && clearInterval(this.timer);
+      const endTime = new Date().getTime();
+      const time = (endTime - this.starTime) / 1000;
+      console.log(chalk.yellow(' 编译完成'));
+      console.log(chalk.yellow('编译用时：' + time + '秒'));
     });
   }
 }
